ff94c3f5cec88700d16db946721e66d48b24060c
dmiedema <danielmiedema+github@me.com>
2016-04-13 19:25:47 -0700

    Remove trailing white space

--- user-provision.sh
+++ user-provision.sh
@@ -9,24 +9,24 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 
 if [[ ! -d "$HOME/.zgen" ]]; then
   git clone https://github.com/tarjoilija/zgen ~/.zgen
-else 
+else
   zgen update
 fi
 
 # Clone repos
 if [[ ! -d "$HOME/src/vimrc" ]]; then
   git clone https://github.com/dmiedema/vimrc.git ~/src/vimrc
-else 
+else
   pushd ~/src/vimrc; git pull; popd
 fi
 if [[ ! -d "$HOME/src/zshrc" ]]; then
   git clone https://github.com/dmiedema/zshrc.git ~/src/zshrc
-else 
+else
   pushd ~/src/zshrc; git pull; popd;
 fi
 if [[ ! -d "$HOME/src/tmux" ]]; then
   git clone https://github.com/dmiedema/tmux.conf.git ~/src/tmux
-else 
+else
   pushd ~/src/tmux; git pull; popd;
 fi
 
